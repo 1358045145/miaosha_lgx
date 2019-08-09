ae3ca5d72a95f1228144e3c55f91e4b9d541a55a
lgx <1358045145@qq.com>
2019-08-09 11:04:52 +0800

    add cache

--- src/main/java/com/yonyou/miaosha/constant/CookieConstant.java
+++ src/main/java/com/yonyou/miaosha/constant/CookieConstant.java
@@ -1,7 +1,12 @@
 package com.yonyou.miaosha.constant;
 
+import java.util.UUID;
+
 public interface CookieConstant {
 
     Integer EXPIRE = 24 * 60; //1天
     String COOKIE_NAME = "cookie_token";
+
+
+
 }
--- src/main/java/com/yonyou/miaosha/constant/RedisConstant.java
+++ src/main/java/com/yonyou/miaosha/constant/RedisConstant.java
@@ -1,10 +1,13 @@
 package com.yonyou.miaosha.constant;
 
+import java.util.UUID;
+
 public interface RedisConstant {
 
     public static final Integer EXPIRE = 30 *3600;
 
-
+    String GOODS_DETAIL_KEY = UUID.randomUUID().toString().replace("-"," "); //商品详情页的KEY
+    String GOODS_LIST_KEY = UUID.randomUUID().toString().replace("-"," "); //商品列表的KEY
 
 
 
